1f3390c67c479d9b3b27e4fc39c7510db462e859
itpark <a.m.smyslov@yandex.ru>
2021-07-28 21:05:22 +0300

    get random phrase

--- src/com/company/PromoPhrasesGenerator.java
+++ src/com/company/PromoPhrasesGenerator.java
@@ -1,6 +1,10 @@
 package com.company;
 
+import java.util.Random;
+
 public class PromoPhrasesGenerator {
+    private Random random = null;
+
     private PhraseList callToActionPhrases = null;
     private PhraseList itemPhrases = null;
     private PhraseList brandPhrases = null;
@@ -8,6 +12,8 @@ public class PromoPhrasesGenerator {
     private PhraseList placePhrases = null;
 
     public PromoPhrasesGenerator() {
+        random = new Random();
+
         callToActionPhrases = new PhraseList();
         callToActionPhrases.addRange(new String[]{
                 "Купи",
@@ -49,6 +55,23 @@ public class PromoPhrasesGenerator {
                 "магазине",
                 "базаре"
         });
+    }
+
+    public String getRandomPhrase() throws Exception{
+        int callToActionIndex = random.nextInt(callToActionPhrases.getLength());
+        String callToAction = callToActionPhrases.getByIndex(callToActionIndex);
+
+        int itemIndex = random.nextInt(itemPhrases.getLength());
+        String item = itemPhrases.getByIndex(itemIndex);
+
+        int brandIndex = random.nextInt(brandPhrases.getLength());
+        String brand = brandPhrases.getByIndex(brandIndex);
+
+        int prepositionIndex = random.nextInt(prepositionPhrases.getLength());
+        String preposition = prepositionPhrases.getByIndex(prepositionIndex);
+
+        int placeIndex = random.nextInt(placePhrases.getLength());
+        String place = placePhrases.getByIndex(placeIndex);
     }
 
 }
